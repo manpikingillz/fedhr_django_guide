5edaf7313af16a7fbcfca5e5d81a9696cc6e17e5
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-12-15 18:21:18 +0300

    added pagination

--- fedhr/employee/graph/queries.py
+++ fedhr/employee/graph/queries.py
@@ -5,13 +5,22 @@ from fedhr.employee.graph.types import EmployeeType
 
 
 class EmployeeQueries(graphene.ObjectType):
-    employees = graphene.List(EmployeeType)
+    employees = graphene.List(
+        EmployeeType,
+        limit=graphene.Int(),
+        offset=graphene.Int())
     employee_by_first_name = graphene.Field(
         EmployeeType, first_name=graphene.String())
 
     # Resolvers
-    def resolve_employees(self, info):
-        return Employee.objects.all()
+    def resolve_employees(self, info, limit=None, offset=None,):
+        queryset = Employee.objects.all().order_by('-id')
+
+        if limit and offset:
+            end = offset + limit
+            queryset = queryset[offset:end]
+
+        return queryset
 
     def resolve_employee_by_first_name(self, info, first_name):
         try:
